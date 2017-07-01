f4644b798f7e8dc8b8e3d32b0c82816de1067682
evlevel <evlevel@stthomas.edu>
2017-07-01 15:13:59 -0500

    Roll added

--- SkunkAppTDD/src/SkunkApp.java
+++ SkunkAppTDD/src/SkunkApp.java
@@ -2,10 +2,33 @@
 public class SkunkApp
 {
 
+	private int numberOfPlayers;
+	private String[] playerNames = new String[2];
+
+	public void getPlayerInfo()
+	{
+		numberOfPlayers = 2;
+		playerNames= new String[numberOfPlayers];
+		
+		playerNames[0] = "Player 1";
+		playerNames[1] = "Player 2";
+		
+	}
+	
 	public static void main(String[] args)
 	{
 		
-
 	}
+
+	public int getNumberOfPlayers()
+	{
+		return numberOfPlayers;
+	}
+
+	public String getPlayerName(int playerNumber)
+	{	
+		return playerNames[playerNumber-1];
+	}
+
 
 }
--- SkunkAppTDD/src/TestSkunkApp.java
+++ SkunkAppTDD/src/TestSkunkApp.java
@@ -1,14 +1,36 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSkunkApp
 {
+	private SkunkApp game;
 
-	@Test
-	public void test()
+	@Before
+	public void setUp()
 	{
-		fail("Not yet implemented");
+		game = new SkunkApp();
 	}
+	
+	@Test
+	public void testGetPlayerInfo()
+	{
+		game.getPlayerInfo();
+		assertEquals(2,game.getNumberOfPlayers());
+		assertEquals("Player 1",game.getPlayerName(1));
+		assertEquals("Player 2",game.getPlayerName(2));
+	}
+	
+	@Test
+	public void canCreateRoll()
+	{
+		Roll roll = new Roll();
+		assertTrue(roll.getLastTotal()<= 12);
+		assertTrue(roll.getLastTotal()>=2);
+
+	}
+	
+	
 
 }
